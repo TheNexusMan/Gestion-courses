86abe15de37b4fd86a8a86076a14282aca610e39
p1707147 <arnaud.debrabant@etu.univ-lyon1.fr>
2019-04-15 17:19:14 +0200

    Correction bug

--- testLogin.php
+++ testLogin.php
@@ -1,74 +1,60 @@
 <?php
+session_start();
 
 $user = 'root';
 $mdp = '';
 $machine = 'localhost';
-$bd = 'projetbdw';
-
-echo "TEST";
-
-
+$bd = 'bdw1';
 
 if((isset($_POST['pseudoInput'])) && (isset($_POST['inputPw'])))
 {
-
-    echo "TEST";
     $connexion = mysqli_connect($machine,$user,$mdp,$bd); //On se connecte
-    echo "TEST";
 
     if(mysqli_connect_errno()) //erreur si > 0
     {
         printf("Echec de la connexion : %s", mysqli_connect_errno());
-    } else
-    {
-        $pseudo = "'" . $_POST['pseudoInput'] . "'";
+    } else {
+
+        $pseudo = $_POST['pseudoInput'];
         $pw = $_POST['inputPw'];
-        echo "<br/>";
-        
-        echo $pseudo;
-        echo "<br/>";
-        echo $pw;
-       $requete = "SELECT * FROM user WHERE pseudo =\'$pseudo\' AND mdp=$pw"; 
-       
-      // echo($requete);
-       $resultat = mysqli_query($connexion, $requete); //Envoie de la requete
-       echo $resultat;
 
-       /*if(mysqli_num_rows($resultat)==0) //Test si le nom && mdp existe
-       {
-           //Si non => Erreur
-           //printf("Echec de la requête");
-           //Renvoie sur la page de connexion
-           header('Location : http://localhost/projet-bdw1/index.php');
-           //exit();
-       } else
-       {
-           while($nuplet = mysqli_fetch_assoc($resultat))
-           {
-               $id_user = $nuplet['id_user'];
-               $id_adherent = $nuplet['id_adherent'];
-               $type = $nuplet['type'];
-               $mdp = $nuplet['mdp'];
-               $pseudo = $nuplet['pseudo'];
-           }
+        $requete = "SELECT * FROM user WHERE pseudo = $pseudo AND mdp = $pw"; 
 
-           if($type == "Admin")
-           {
-               //Renvoie sur la page admin suivante 
-               header('Location : http://localhost/projet-bdw1/index.php');
-              // exit();
-           }
-           else {
-                //Renvoie sur la page User suivante
-                header('Location : http://localhost/projet-bdw1/index.php');
-                //exit();
-           }
+        $resultat = mysqli_query($connexion, $requete); //Envoie de la requete
 
+        echo mysqli_num_rows($resultat);
 
-       }*/
+    //    if(mysqli_num_rows($resultat)==0) //Test si le nom && mdp existe
+    //    {
+    //        //Si non => Erreur
+    //        //printf("Echec de la requête");
+    //        //Renvoie sur la page de connexion
+    //        header('Location : http://localhost/projet-bdw1/index.php');
+    //        //exit();
+    //    } else
+    //    {
+    //        while($nuplet = mysqli_fetch_assoc($resultat))
+    //        {
+    //            $_SESSION['id_user'] = $nuplet['id_user'];
+    //            //$id_adherent = $nuplet['id_adherent'];
+    //            $type = $nuplet['type'];
+    //            //$mdp = $nuplet['mdp'];
+    //            //$pseudo = $nuplet['pseudo'];
+    //        }
+
+    //        if($type == "Admin")
+    //        {
+    //            //Renvoie sur la page admin suivante 
+    //            header('Location : http://localhost/projet-bdw1/index.php');
+    //           // exit();
+    //        }
+    //        else {
+    //             //Renvoie sur la page User suivante
+    //             header('Location : http://localhost/projet-bdw1/adherent.php');
+    //             //exit();
+    //        }
+    //    }
    }
-   
-
 } 
 
 ?>
