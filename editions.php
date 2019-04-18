ed82b717b0b3c7a2e1f69705860854f34ce7aa2a
D <f>
2019-04-18 18:31:54 +0200

    Valeurs en fonction du genre...

--- editions.php
+++ editions.php
@@ -8,6 +8,35 @@
     $connexion = mysqli_connect($machine,$user,$mdp, $bd);
 
 
+   
+
+    //créer un bouton pour changer le sexe choisis
+
+    if(isset($_POST['genderSelect']))
+    {
+        $chosenSex = $_POST['genderSelect'];
+      //  print $_POST['genderSelect'] . "Valeur apres form";
+    }else
+    {
+        $chosenSex = "H";
+      //  print $chosenSex . " Valeur par défaut";
+    }
+
+    $idCourse = 1; //A changer via l'appel dans courses.php
+
+    if($chosenSex == "H")
+    {
+        print "<div class='container'>
+                    Sexe sélectionnée : Hommes
+                <div>";
+    } else
+    {
+        print "<div class='container'>
+                 Sexe sélectionnée : Femmes
+                <div>";
+    }
+
+
     if(mysqli_connect_errno()) // erreur si > 0
     printf("Échec de la connexion : %s", mysqli_connect_error());
     else {
@@ -19,14 +48,13 @@
                                     JOIN participation pa ON pa.dossard = tmp.dossard
                                     JOIN adherent adh ON pa.id_adherent = adh.id_adherent
                                     JOIN resultat res ON res.id_epreuve = ep.id_epreuve
-                    WHERE ed.id_course = 1 AND adh.sexe = 'H'
-                    GROUP BY ed.annee ";
+                    WHERE ed.id_course = $idCourse AND adh.sexe = '$chosenSex'
+                    GROUP BY ed.annee";
         //A definir $idCourse pour savoir quelle course est selectionée
         //Permet d'avoir la moyenne de temps en fonction du sexe (H = Homme, F = Femmes)
 
         $resultat = mysqli_query($connexion, $requete);
-
-        print "<section class='listeEditionAdherent'>
+        print "<section class='listeEditions'>
         <div class='container'>
             <table class='table'>
                 <thead>
@@ -35,9 +63,10 @@
                         <th scope='col'>Nombre de participants Total</th>
                         <th scope='col'>Meilleur temps</th>
                         <th scope='col'>Nombre de club représenté</th>
+                        <th scope='col'>Moyenne de temps </th>
                     </tr>
                 </thead>
-                <tbody>";
+            <tbody>";
 
         while ($nuplet = mysqli_fetch_assoc($resultat)) {
             $annee = $nuplet['annee'];
@@ -53,32 +82,53 @@
                         <td>$nb_participants</td>
                         <td>$meilleur_temps min</td>
                         <td>$nb_club</td>;
-                    </tr>"
+                        <td>$moyenneTmp min</td>
+                    </tr>";
             // print '<td><div class="d-flex flex-row bd-highlight">';
             // print ' <div class="p-2 bd-highlight">';
             // print '<button type="button" class="btn btn-outline-success" id="edit">Edit</button></div></div></td>
             //     </tr>';
             
-                }
+        
+
+        }
 
 
-            print "             </tbody>
-                            </table>
-                        </div>
-                </section>";
     }
+
+
+
+
+
+?>
+                    </tbody>
+                </table>
+                </div>
+                
+        <div class="d-flex flex-row justify-content-center">
+        <form method="POST" action="editions.php">
+            <div class="form-group">
+            <label for="genderSelect">Filtrer par genre :</label>
+            <select class="form-control" id="genderSelect" name="genderSelect">
+                <option>H</option>
+                <option>F</option>
+            </select>
+            </div>
+            <button type="submit" class="btn btn-success" name="changeGender">Valider</button>
+
+        </form>
+        </div>
+
+</section>
+
+
+
+<?php
+
+
+
+
     mysqli_close($connexion);
-
-
-
-
-
-
-
-
-
-
-
 
     include "includes/footer.php";
 ?>
