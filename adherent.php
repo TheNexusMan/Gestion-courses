c39406e2999613f916383cdf9f00b52c2f29d1a8
p1707147 <arnaud.debrabant@etu.univ-lyon1.fr>
2019-04-19 17:03:07 +0200

    Finalisation du back de adherent

--- adherent.php
+++ adherent.php
@@ -12,6 +12,19 @@
     else {
         $idUser = $_SESSION['id_adherent'];
 
+        if(isset($_POST['nom']))
+        {
+            $requete = 'UPDATE adherent
+                        SET nom = "' . $_POST['nom'] . '", prenom = "' . $_POST['prenom'] . '", date_naissance = "' . $_POST['naissance'] . '", sexe = "' . $_POST['sexe'] . '", adresse = "' . $_POST['adresse'] . '", date_certif_club = "' . $_POST['dateClub'] . '", club = "' . $_POST['nomClub'] . '"
+                        WHERE id_adherent = ' . $idUser;
+
+            $resultat = mysqli_query($connexion, $requete);
+
+            if($resultat == FALSE){
+                printf("Échec de la requête de mise à jour");
+            }
+        }
+
         $requete = "SELECT *
                     FROM adherent
                     WHERE id_adherent = $idUser";
@@ -19,7 +32,7 @@
         $resultat = mysqli_query($connexion, $requete);
 
         if($resultat == FALSE)
-            printf("Échec de la requête");
+            printf("Échec de la requête de récupération des informations d'adhérent");
         else {
             //Affichage des informations de l'adhérent
             while ($nuplet = mysqli_fetch_assoc($resultat)) {
@@ -32,46 +45,68 @@
                 $nomClub = $nuplet['club'];
                 print "<section class='adherent'>
                             <div class='adherentInfos container'>
-                                <div class='adherentInfosBloc container mx-auto col-8 mw-50'>
-                                    <div class='row ligneInfo'>
-                                        <div class='col-4'>
-                                            <p class='nomInfo'>Nom :</p>
-                                            <p>$nom</p>
+                                <div id='adherentInfosBloc' class='adherentInfosBloc container mx-auto col-8 mw-50'>
+                                    <form action='' method='POST'>
+                                        <div class='row ligneInfo'>
+                                            <div class='col-4'>
+                                                <p class='nomInfo'>Nom :</p>
+                                                <p id='nomAdherent' class='readInfoAdherent'>$nom</p>
+                                                <input type='text' id='nomAdherentInput' class='writeInfoAdherent' name='nom' value='$nom'>
+                                            </div>
+                                            <div class='col-4'></div>
+                                            <div class='col-4'>
+                                                <p class='nomInfo'>Prenom :</p>
+                                                <p id='prenomAdherent' class='readInfoAdherent'>$prenom</p>
+                                                <input type='text' id='prenomAdherentInput' class='writeInfoAdherent' name='prenom' value='$prenom'>
+                                            </div>
                                         </div>
-                                        <div class='col-4'></div>
-                                        <div class='col-4'>
-                                            <p class='nomInfo'>Prenom :</p>
-                                            <p>$prenom</p>
+                                        <div class='row ligneInfo'>
+                                            <div class='col-4'>
+                                                <p class='nomInfo'>Naissance :</p>
+                                                <p id='naissanceAdherent' class='readInfoAdherent'>$dateNaissance</p>
+                                                <input type='text' id='naissanceAdherentInput' class='writeInfoAdherent' name='naissance' value='$dateNaissance'>
+                                            </div>
+                                            <div class='col-4'></div>
+                                            <div class='col-4'>
+                                                <p class='nomInfo'>Sexe :</p>
+                                                <p id='sexeAdherent' class='readInfoAdherent'>$sexe</p>
+                                                <input type='text' id='sexeAdherentInput' class='writeInfoAdherent' name='sexe' value='$sexe'>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class='row ligneInfo'>
-                                        <div class='col-4'>
-                                            <p class='nomInfo'>Naissance :</p>
-                                            <p>$dateNaissance</p>
+                                        <div class='row ligneInfo'>
+                                            <div class='col-8'>
+                                                <p class='nomInfo'>Adresse :</p>
+                                                <p id='adresseAdherent' class='readInfoAdherent'>$adresse</p>
+                                                <input type='text' id='adresseAdherentInput' class='writeInfoAdherent' name='adresse' value='$adresse'>
+                                            </div>
                                         </div>
-                                        <div class='col-4'></div>
-                                        <div class='col-4'>
-                                            <p class='nomInfo'>Sexe :</p>
-                                            <p>$sexe</p>
+                                        <div class='row ligneInfo'>
+                                            <div class='col-4'>
+                                                <p class='nomInfo'>dateClub :</p>
+                                                <p id='dateClubAdherent' class='readInfoAdherent'>$dateClub</p>
+                                                <input type='text' id='dateClubAdherentInput' class='writeInfoAdherent' name='dateClub' value='$dateClub'>
+                                            </div>
+                                            <div class='col-4'></div>
+                                            <div class='col-4'>
+                                                <p class='nomInfo'>nomClub :</p>
+                                                <p id='nomClubAdherent' class='readInfoAdherent'>$nomClub</p>
+                                                <input type='text' id='nomClubAdherentInput' class='writeInfoAdherent' name='nomClub' value='$nomClub'>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class='row ligneInfo'>
-                                        <div class='col-8'>
-                                            <p class='nomInfo'>Adresse :</p>
-                                            <p>$adresse</p>
+                                        <div class='row ligneButton readInfoAdherent readInfoAdherentFlex' id='modifInfoAdherent'>
+                                            <div class='col-8'></div>
+                                            <button type='button' class='col-2'>Modifier</button>
+                                            <div class='col-2'></div>
                                         </div>
-                                    </div>
-                                    <div class='row ligneInfo'>
-                                        <div class='col-4'>
-                                            <p class='nomInfo'>dateClub :</p>
-                                            <p>$dateClub</p>
+                                        <div class='row ligneButton writeInfoAdherent writeInfoAdherentFlex' id='modifInfoAdherent'>
+                                            <div class='col-6'>
+                                            </div>
+                                            <button type='submit' class='col-2'>Valider</button>
+                                            <button type='button' id='annulerInfoAdherent' class='col-2'>Annuler</button>
+                                            <div class='col-2'>
+                                            </div>
                                         </div>
-                                        <div class='col-4'></div>
-                                        <div class='col-4'>
-                                            <p class='nomInfo'>nomClub :</p>
-                                            <p>$nomClub</p>
-                                        </div>
-                                    </div>
+                                    </form>
                                 </div>
                             </div>
                         </section>";
@@ -124,3 +159,38 @@
 
     include "includes/footer.php";
 ?>
+
+<script>
+    document.getElementById("modifInfoAdherent").onclick = afficheFormAdherent;
+    document.getElementById("annulerInfoAdherent").onclick = annulerFormAdherent;
+
+    const nom = document.getElementById('nomAdherent').innerHTML;
+    const prenom = document.getElementById('prenomAdherent').innerHTML;
+    const naissance = document.getElementById('naissanceAdherent').innerHTML;
+    const sexe = document.getElementById('sexeAdherent').innerHTML;
+    const adresse = document.getElementById('adresseAdherent').innerHTML;
+    const dateClub = document.getElementById('dateClubAdherent').innerHTML;
+    const nomClub = document.getElementById('nomClubAdherent').innerHTML;
+
+    function afficheFormAdherent()
+    {
+        Array.from(document.getElementsByClassName('writeInfoAdherent')).forEach(n => n.style.display = "inline-block");
+        Array.from(document.getElementsByClassName('writeInfoAdherentFlex')).forEach(n => n.style.display = "flex");
+        Array.from(document.getElementsByClassName('readInfoAdherent')).forEach(n => n.style.display = "none");
+    }
+
+    function annulerFormAdherent()
+    {
+        Array.from(document.getElementsByClassName('writeInfoAdherent')).forEach(n => n.style.display = "none");
+        Array.from(document.getElementsByClassName('readInfoAdherent')).forEach(n => n.style.display = "inline-block");
+        Array.from(document.getElementsByClassName('readInfoAdherentFlex')).forEach(n => n.style.display = "flex");
+
+        document.getElementById('nomAdherentInput').value = nom;
+        document.getElementById('prenomAdherentInput').value = prenom;
+        document.getElementById('naissanceAdherentInput').value = naissance;
+        document.getElementById('sexeAdherentInput').value = sexe;
+        document.getElementById('adresseAdherentInput').value = adresse;
+        document.getElementById('dateClubAdherentInput').value = dateClub;
+        document.getElementById('nomClubAdherentInput').value = nomClub;
+    }
+</script>
